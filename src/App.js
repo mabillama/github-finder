eb5567f7136055c7f69aae4f31c50de164f88bf9
Marc Abillama <mabillama@gmail.com>
2022-01-28 11:42:31 +0100

    Initializing...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-purple-500">
+      <header></header>
+      <body>
+        <h1 className="text-white text-3xl font-bold underline ">
+          Hello world!
+        </h1>
+        <button class="btn btn-primary">daisyUI Button</button>
+      </body>
     </div>
   );
 }
